1b846d37f4effbe2bd4452c2bb67ebefcffddf57
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-17 08:49:12 -0600

    Added error and success handlers. Opened the db with title QuizQuestDatabase version 1.

--- Practice/app.js
+++ Practice/app.js
@@ -5,3 +5,13 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 if (!window.indexedDB) {
   alert('Uh oh! Looks like indexedDB isn\'t supported.');
 }
+
+let request = window.indexedDB.open('QuizQuestDatabase', 1), db, tx, store, index;
+
+request.onerror = function(e) {
+  console.error('There was an error: ', e.target.errorCode);
+};
+
+request.onsuccess = function(e) {
+  db = request.result;
+};
