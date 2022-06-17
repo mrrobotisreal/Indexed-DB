cf17d25f02c00254870a4c970fcac8a4ce643170
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-17 08:54:45 -0600

    Added onupgradeneeded handler, created objectStore within, added index on questionText.

--- Practice/app.js
+++ Practice/app.js
@@ -8,6 +8,12 @@ if (!window.indexedDB) {
 
 let request = window.indexedDB.open('QuizQuestDatabase', 1), db, tx, store, index;
 
+request.onupgradeneeded = function(e) {
+  let db = request.result, store = db.createObjectStore('QuestionsStore', {keyPath: 'qID'}), index = store.createIndex('questionText', 'questionText', {unique: false});
+  // store = db.createObjectStore('QuestionsStore', {autoIncrement: true});
+
+};
+
 request.onerror = function(e) {
   console.error('There was an error: ', e.target.errorCode);
 };
